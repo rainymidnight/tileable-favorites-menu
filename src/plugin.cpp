6589e39d49578993785255663b96636d2ba0f5d1
Rainy <rainymidnighthighway@gmail.com>
2026-07-26 00:52:16 +0200

    Bump version to 1.0.1

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -33,6 +33,6 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse)
         return false;
     }
 
-    logger::info("Tileable Favorites Menu 1.0.0 loaded");
+    logger::info("Tileable Favorites Menu 1.0.1 loaded");
     return true;
 }
